9f9ec0c7a90150ea3d9f4f5f6d3572888be6c326
Richard Rascher-Friesenhausen <richard@rrfriesenhausen-nb.fme.lan>
2020-10-14 21:57:49 +0200

    Use Module PIL.Image in MyImage.py

--- MyImage.py
+++ MyImage.py
@@ -1,13 +1,13 @@
-#=======================================================================
+# =======================================================================
 # begin of file *MyImage.py*
-#=======================================================================
+# =======================================================================
 # richard@mevis.de (c) 2005-2011
 
 """Modul MyImage.
 """
 
 # history
-#=======================================================================
+# =======================================================================
 # sept 2005: very first version
 # 20-10-2005: added and corrected comments
 # 11-11-2005: added comments, added member fcn stats in MyImage
@@ -19,31 +19,34 @@
 # 24-02-2011: some new members
 # 01-04-2011: add split and merge fcns for rgb images
 # 07-04-2011: save gray value images correctly
-# 14-04-2011: add psnr fcn 
+# 14-04-2011: add psnr fcn
 # 15-04-2011: correct syntax error and default values in imnoise (gauss)
 # 16-04-2011: use module Image in save-member
 # 17-04-2011: clamp pixel values in imnoise
 # 15-12-2018: print as function -> Python 3
+# 14-10-2020: use imageio's imread, imwrite
 
 # module imports
-#=======================================================================
+# =======================================================================
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-import scipy.misc as msc
+#import scipy.misc as msc
+from PIL import Image
 import scipy.ndimage.filters as flt
 
 from math import log10
 
 # global vars
-#=======================================================================
-__Version = 2.2
+# =======================================================================
+__Version = 2.3
 
 # class defs
-#=======================================================================
+# =======================================================================
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 # begin of class *MyImage*
+
 
 class MyImage(object):
     """Klasse MyImage.
@@ -61,7 +64,7 @@ class MyImage(object):
     * das Berechnen von Mittelwert und Standardabweichung,
     * das Darstellen des Bildes.     
     """
-    
+
     def __init__(self, *args, **kwargs):
         """Konstruktor.
 
@@ -76,15 +79,17 @@ class MyImage(object):
         3. img = MyImage(bild)
             erzeugt eine Kopie eines Objektes 'bild' der Klasse *MyImage*.
         """
-        
+
         # self.__img is an numpy array of type float
         # self._width, self.__height are the dimensions of the image
 
         if len(args) == 1:
             # read an image from a file
             if isinstance(args[0], str):
-                self.__img = msc.imread(args[0]).astype(np.float32)
-            # create an image from an numpy array    
+                #                self.__img = msc.imread(args[0]).astype(np.float32)
+                img = Image.open(args[0])
+                self.__img = np.asarray(img).astype(np.float32)
+            # create an image from an numpy array
             if isinstance(args[0], np.ndarray):
                 self.__img = args[0].copy().astype(np.float32)
             # create an image from a MyImage object
@@ -92,8 +97,8 @@ class MyImage(object):
                 self.__img = args[0].__img.copy()
         # create constant image with given dimensions
         if len(args) == 3:
-            self.__img = args[2]*np.ones((args[1],args[0]),dtype=np.float32)
-            
+            self.__img = args[2]*np.ones((args[1], args[0]), dtype=np.float32)
+
         # read dimensions
         # XXX: read RGB, RGBA images! depth!
         self.__height, self.__width = self.__img.shape[:2]
@@ -117,13 +122,13 @@ class MyImage(object):
         die Position in das Bild hineingespiegelt.
         """
         try:
-            g = self.__img[y,x]
+            g = self.__img[y, x]
         except:
             if not(0 <= x < self.__width):
-                x = (self.__width-1)  - (x%self.__width)
+                x = (self.__width-1) - (x % self.__width)
             if not(0 <= y < self.__height):
-                y = (self.__height-1) - (y%self.__height)
-            g = self.__img[y,x]
+                y = (self.__height-1) - (y % self.__height)
+            g = self.__img[y, x]
 
         return g
 
@@ -134,9 +139,9 @@ class MyImage(object):
         Grauwert 'g' eingesetzt. Liegt die Position ausserhalb des Bildes,
         gibt es einen Fehler.
         """
-        self.__img[y,x] = value
+        self.__img[y, x] = value
         return
-    
+
     def __getitem__(self, index):
         """Pixel auslesen.
 
@@ -144,7 +149,7 @@ class MyImage(object):
         Liegt die Position ausserhalb des Bildes, so wird die Position in das
         Bild hineingespiegelt.
         """
-        return self.getPixel(index[0],index[1])
+        return self.getPixel(index[0], index[1])
 
     def __setitem__(self, index, value):
         """Pixel setzen.
@@ -153,7 +158,7 @@ class MyImage(object):
         'g' eingesetzt. Liegt die Position ausserhalb des Bildes, gibt es einen
         Fehler.
         """
-        self.setPixel(index[0],index[1],value)
+        self.setPixel(index[0], index[1], value)
         return
 
     # dimension access (read only)
@@ -163,7 +168,7 @@ class MyImage(object):
         Mit breite = img.getWidth() erhaelt man die Breite des Bildes.
         """
         return self.__width
-    
+
     def getHeight(self):
         """Bildhoehe lesen.
 
@@ -190,9 +195,10 @@ class MyImage(object):
         'dateiname' geschrieben. Die Endung von 'dateiname' bestimmt das
         Bildformat.
         """
-        if len(self.__img.shape) == 2: # gray
-#            cmap = plt.cm.gray
-            img = msc.toimage(self.__img.astype('uint8'))
+        if len(self.__img.shape) == 2:  # gray
+            #            cmap = plt.cm.gray
+            #            img = msc.toimage(self.__img.astype('uint8'))
+            img = Image.fromarray(self.__img.astype('uint8'))
             img.save(filename)
 #            plt.imsave(filename, self.__img.astype('uint8'), cmap=cmap, vmin=0, vmax=255)
 #            msc.imsave(filename, self.__img)  # scales the image!!!
@@ -207,12 +213,12 @@ class MyImage(object):
         Der Befehl img.show() zeigt das Bild in einem Fenster an.
         """
 
-        if len(self.__img.shape) == 2: # gray
+        if len(self.__img.shape) == 2:  # gray
             cmap = plt.cm.gray
-            plt.imshow(self.__img, cmap=cmap, interpolation='nearest',\
+            plt.imshow(self.__img, cmap=cmap, interpolation='nearest',
                        vmin=0, vmax=255)
-        else: # color
-            plt.imshow(self.__img/255, interpolation='nearest',\
+        else:  # color
+            plt.imshow(self.__img/255, interpolation='nearest',
                        vmin=0, vmax=1)
 
         plt.axis('off')
@@ -231,20 +237,20 @@ class MyImage(object):
         Mit H = img.abs_histogram() erhaelt man das absolute Histogramm
         des Bildes 'img' in der Liste 'H' zurueck.
         """
-        return np.histogram(self.__img, 256, (0,256))[0]
-    
+        return np.histogram(self.__img, 256, (0, 256))[0]
+
     def relHistogram(self):
         """Relatives Histogramm berechnen.
 
         Mit h = img.rel_histogram() erhaelt man das relative Histogramm
         des Bildes 'img' in der Liste 'h' zurueck.
         """
-        return np.histogram(self.__img, 256, (0,256), True)[0]
+        return np.histogram(self.__img, 256, (0, 256), True)[0]
 
     def cumHistogram(self):
         H = self.relHistogram()
         C = H.copy()
-        for i in range(1,len(C)):
+        for i in range(1, len(C)):
             C[i] = C[i-1]+C[i]
         return C
 
@@ -260,71 +266,87 @@ class MyImage(object):
         return self.__img.min(), self.__img.max()
 
 # end of class *MyImage*
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 # fcn defs
-#=======================================================================
+# =======================================================================
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 # noise fcn coded along imnoise.m from Matlab
 
+
 def imnoise(img, name, *args):
-  """Verrauscht eine gegebenes Bild.
+    """Verrauscht eine gegebenes Bild.
 
-  1. out = imnoise(img,'gauss',mu,nu)
-    verrauscht nach Gauss mit Mittelwert *mu* und Varianz *nu*.
-  2. out = imnoise(img,'salt',dense)
-    verrauscht mit Salt und Pepper mit Dichte *dense*.
-  3. out = imnoise(img,'speckle',intense)
-    verrauscht mit Speckle und Intensitaet *intense*.
-  """
-  out = MyImage(img)
-  if name[:2] == 'ga':
-    if   len(args) == 2: mu, var = args
-    elif len(args) == 1: mu, var = args[0], 0.5*255
-    else:                mu, var = 0, 0.5*255
-    sigma = var**0.5
-    
-    for y in range(out.getHeight()):
-      for x in range(out.getWidth()):
-        r = random.gauss(mu,sigma)
-        g = out[x,y] = out[x,y] + r
-        if g < 0.:   out[x,y] = 0.
-        if g > 255.: out[x,y] = 255.
-        
-  elif name[:2] == 'sa':
-    if len(args) == 1: dense = args[0]
-    else:              dense = 0.05
-    denseh = 0.5*dense
-    
-    for y in range(out.getHeight()):
-      for x in range(out.getWidth()):
-        r = random.random()
-        if       r < denseh: out[x,y] = 0
-        elif 1.-denseh <= r: out[x,y] = 255
-        else:                pass
-        
-  elif name[:2] == 'sp':
-    if len(args) == 1: intense = args[0]
-    else:              intense = 0.04
+    1. out = imnoise(img,'gauss',mu,nu)
+      verrauscht nach Gauss mit Mittelwert *mu* und Varianz *nu*.
+    2. out = imnoise(img,'salt',dense)
+      verrauscht mit Salt und Pepper mit Dichte *dense*.
+    3. out = imnoise(img,'speckle',intense)
+      verrauscht mit Speckle und Intensitaet *intense*.
+    """
+    out = MyImage(img)
+    if name[:2] == 'ga':
+        if len(args) == 2:
+            mu, var = args
+        elif len(args) == 1:
+            mu, var = args[0], 0.5*255
+        else:
+            mu, var = 0, 0.5*255
+        sigma = var**0.5
 
-    factor = (12*intense)**0.5
-    for y in range(out.getHeight()):
-      for x in range(out.getWidth()):
-        r = random.random()-0.5
-        g = out[x,y] = out[x,y] * (1 + factor*r)
-        if g < 0.:   out[x,y] = 0.
-        if g > 255.: out[x,y] = 255.
+        for y in range(out.getHeight()):
+            for x in range(out.getWidth()):
+                r = random.gauss(mu, sigma)
+                g = out[x, y] = out[x, y] + r
+                if g < 0.:
+                    out[x, y] = 0.
+                if g > 255.:
+                    out[x, y] = 255.
 
-  else:
-    print("unknown noise type")
+    elif name[:2] == 'sa':
+        if len(args) == 1:
+            dense = args[0]
+        else:
+            dense = 0.05
+        denseh = 0.5*dense
 
-  return out
+        for y in range(out.getHeight()):
+            for x in range(out.getWidth()):
+                r = random.random()
+                if r < denseh:
+                    out[x, y] = 0
+                elif 1.-denseh <= r:
+                    out[x, y] = 255
+                else:
+                    pass
+
+    elif name[:2] == 'sp':
+        if len(args) == 1:
+            intense = args[0]
+        else:
+            intense = 0.04
+
+        factor = (12*intense)**0.5
+        for y in range(out.getHeight()):
+            for x in range(out.getWidth()):
+                r = random.random()-0.5
+                g = out[x, y] = out[x, y] * (1 + factor*r)
+                if g < 0.:
+                    out[x, y] = 0.
+                if g > 255.:
+                    out[x, y] = 255.
+
+    else:
+        print("unknown noise type")
+
+    return out
 
 #
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+
 
 def split(img):
     """Return color channels of an RGB image.
@@ -332,9 +354,9 @@ def split(img):
 
     if img.getDepth() == 3:
         arr = img._getImage()
-        r = MyImage(arr[:,:,0])
-        g = MyImage(arr[:,:,1])
-        b = MyImage(arr[:,:,2])
+        r = MyImage(arr[:, :, 0])
+        g = MyImage(arr[:, :, 1])
+        b = MyImage(arr[:, :, 2])
     else:
         r = img.copy()
         g = img.copy()
@@ -342,17 +364,18 @@ def split(img):
 
     return r, g, b
 
-def merge(r,g,b):
+
+def merge(r, g, b):
     """Combine single RGB channels into a color image.
     """
 
     R, G, B = r._getImage(), g._getImage(), b._getImage()
 
     h, w = r.getHeight(), r.getWidth()
-    RGB = np.zeros((h, w, 3), np.float32) 
-    RGB[:,:,0] = R
-    RGB[:,:,1] = G
-    RGB[:,:,2] = B
+    RGB = np.zeros((h, w, 3), np.float32)
+    RGB[:, :, 0] = R
+    RGB[:, :, 1] = G
+    RGB[:, :, 2] = B
 
     return MyImage(RGB)
 
@@ -370,21 +393,23 @@ def MedianFilter(img, size=3):
     return MyImage(arr_out)
 
 # filter fcn ala matlab.
+
+
 def imfilter(img, mask, boundary='symmetric'):
     """Filtert ein Bild mit der gegebenen Maske.
 
     boundary = 'symmetric' (default), 'replicate', 'circular', value
     """
-    
+
     # create numpy array from image
     if isinstance(img, MyImage):
         arr_img = img._getImage()
     else:
         arr_img = np.array(img, dtype=np.float)
-    
+
     if len(arr_img.shape) > 2:
-        arr_img = arr_img[:,:,0]
-    
+        arr_img = arr_img[:, :, 0]
+
     if boundary == 'symmetric':
         mode, cval = 'reflect', 0.0
     elif boundary == 'replicate':
@@ -399,7 +424,8 @@ def imfilter(img, mask, boundary='symmetric'):
     return MyImage(arr_out)
 
 #
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+
 
 def psnr(img1, img2):
     w, h = img1.getWidth(), img1.getHeight()
@@ -408,15 +434,15 @@ def psnr(img1, img2):
     s = 0.
     for x in range(w):
         for y in range(h):
-            s += (img1[x,y]-img2[x,y])**2
+            s += (img1[x, y]-img2[x, y])**2
     return 10.*log10((Max**2*N)/s)
 
 
 # script entry
-#=======================================================================
+# =======================================================================
 if __name__ == '__main__':
     pass
 
-#=======================================================================
+# =======================================================================
 # end of file *MyImage.py*
-#=======================================================================
+# =======================================================================
